8489e6b984ff38df35878b3cefe148c992e511d0
Hmm <gbreeze.study@gmail.com>
2026-02-08 19:49:54 -0800

    V2.2
    Added FPS counter, frame skipping, and a couple fps optimizations, still no arduino stuff tho

--- BottleTracker/BottleTracker.py
+++ BottleTracker/BottleTracker.py
@@ -29,9 +29,11 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 time.sleep(1)
 
-xres, yres = 640, 480
+xres, yres = 1200, 900 # can also be set to higher (320, 240 / 640, 480 / 1200, 900), 
 outputX, outputY = 90, 90
 cx, cy = xres//2, yres//2
+frameNum = 0
+frameSkip = 5
 
 while cap.isOpened():
     if not cap.grab():
@@ -39,31 +41,35 @@ while cap.isOpened():
     ret, frame = cap.retrieve()
     if not ret:
         break
+    
     prevTime = time.time()
     frame = cv2.resize(frame, (xres, yres))
     results = model(frame, classes=[39], verbose=False)
     annotated_frame = frame.copy()
-    annotated_frame = results[0].plot()
+    #annotated_frame = results[0].plot()
     bottleFound = False
     
-    for r in results:
-        boxes = r.boxes
-        if boxes is not None:
-            for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0].tolist()
-                bottleFound = True
-                center_x = int((x1 + x2)/2)
-                center_y = int((y1 + y2)/2)
-                moveX = int(center_x-xres/2)
-                moveY = int(center_y-yres/2)
-                cv2.circle(annotated_frame, (center_x, center_y), 2, (0, 0, 255), -1)
-
-    cv2.drawMarker(annotated_frame,(cx, cy),color=(0, 255, 0),markerType=cv2.MARKER_CROSS,markerSize=20,thickness=1)
-    currTime = time.time()
-    fps = 1/(currTime-prevTime)
-    prevTime = currTime
-    cv2.putText(annotated_frame, f"FPS: {int(fps)}", (10, 20),cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.imshow('Webcam Detection', annotated_frame)
+    if frameNum >= frameSkip:
+        for r in results:
+            boxes = r.boxes
+            if boxes is not None:
+                for box in boxes:
+                    x1, y1, x2, y2 = box.xyxy[0].tolist()
+                    bottleFound = True
+                    center_x = int((x1 + x2)/2)
+                    center_y = int((y1 + y2)/2)
+                    moveX = int(center_x-xres/2)
+                    moveY = int(center_y-yres/2)
+                    cv2.circle(annotated_frame, (center_x, center_y), 2, (0, 0, 255), -1)
+        cv2.drawMarker(annotated_frame,(cx, cy),color=(0, 255, 0),markerType=cv2.MARKER_CROSS,markerSize=20,thickness=1)
+        currTime = time.time()
+        fps = 1/(currTime-prevTime)
+        cv2.putText(annotated_frame, f"FPS: {int(fps)}", (10, 20),cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+        cv2.imshow('Webcam Detection', annotated_frame)
+        frameNum = 0
+    else:
+        cv2.imshow('Webcam Detection', frame) #delete if you prefer low FPS (no spazzing)
+        frameNum += 1
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
